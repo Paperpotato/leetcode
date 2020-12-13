b1567f4fa12ba606198cd754a043580d6f867c98
Paperpotato <cabbage.leaves@gmail.com>
2020-12-13 21:37:30 +0800

    Update excel-sheet-column-title.js

--- solved-problems/excel-sheet-column-title.js
+++ solved-problems/excel-sheet-column-title.js
@@ -3,25 +3,14 @@
  * @return {string}
  */
 var convertToTitle = function(n) {
-    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    let dict = alphabet.split('')
-    console.log(dict)
     if (n === 0) return null
     if (n < 27) return String.fromCharCode(64 + n)
     let output = ''
 
     while (n > 0) {
-        // let output = ''
         let num = n % 26
         num = num == 0 ? 26: num
-        console.log(num)
-        // if (num > 26) {
-        //     n -= 26
-        //     console.log(n)
-        //     continue
-        // }
         output = String.fromCharCode(64 + num) + output
-        console.log('added', output)
         n -= num
         n /= 26
 
